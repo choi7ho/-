bed182b5281a1d54947c070c24256ec5aa52ee95
choi7ho <113654003+choi7ho@users.noreply.github.com>
2022-10-21 21:52:28 +0900

    Add files via upload

---
+++ 현대엘리베이터/js/main.js
@@ -0,0 +1,47 @@
+$(function () {
+    $('.top_banner_slide').slick({
+        arrows: false,
+        dots: true,
+    });
+
+    $('.top_banner i').on('click', function () {
+        $('.top_banner').slideUp(200)
+        $('.main').addClass('on')
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop()
+        if (sct > 0) {
+            $('.header .bottom').addClass('on')
+        } else {
+            $('.header .bottom').removeClass('on')
+        }
+    });
+
+    $('.header .global').on('click', function () {
+        $('.header .link').toggleClass('on')
+    })
+
+    $('.main_slide').slick({
+        arrows: false,
+        autoplay: true,
+        pauseOnHover: false
+    });
+
+    $('.main_slide').on('afterChange', function (e, s, c) {
+        $('.main_visual .page li').eq(c).addClass('on').siblings().removeClass('on');
+        $('.num strong').text("0" + (c + 1))
+    });
+
+    $('.page li').on('click', function () {
+        var idx = $(this).index()
+        $('.main_slide').slick('slickGoTo', idx)
+    });
+
+    $('.main_visual .arrows i:first-child').on('click', function () {
+        $('.main_slide').slick('slickPrev')
+    });
+    $('.main_visual .arrows i:last-child').on('click', function () {
+        $('.main_slide').slick('slickNext')
+    });
+})
